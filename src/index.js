8d5603eb7490be926f4105517d89280b1240fa5a
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2022-12-01 11:52:36 +0330

    "add js function for two buttons"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards:" + cards[0] + " " + cards[1];
   sumEl.textContent = "Sum:" + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -19,4 +26,11 @@ function startGame() {
     isAlive = false;
   }
   messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+  let card = 7;
+  sum += card;
+  renderGame();
 }
